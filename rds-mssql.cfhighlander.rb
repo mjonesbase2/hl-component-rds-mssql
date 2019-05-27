4677d951838b3db7fb84e818819ca352035a40e3
Tim Hassett <t.hassett@base2services.com>
2019-05-27 15:31:23 +1000

    Basic MS-SQL component

--- rds-mssql.cfhighlander.rb
+++ rds-mssql.cfhighlander.rb
@@ -1,11 +1,21 @@
 CfhighlanderTemplate do
-  Name 'rds-mssql'
-  Description "rds-mssql - #{component_version}"
-
-  Parameters do
-    ComponentParam 'EnvironmentName', 'dev', isGlobal: true
-    ComponentParam 'EnvironmentType', 'development', allowedValues: ['development','production'], isGlobal: true
-  end
-
-
+    Name 'rds-mssql'
+    Description "#{component_name} - #{component_version}"
+    ComponentVersion component_version
+    DependsOn 'vpc'
+  
+    Parameters do
+      ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
+      ComponentParam 'StackOctet', isGlobal: true
+      ComponentParam 'RDSSnapshotID'
+      ComponentParam 'MultiAZ', 'false', allowedValues: ['true','false']
+      ComponentParam 'EnvironmentName', 'dev', isGlobal: true
+      ComponentParam 'EnvironmentType', 'development', isGlobal: true, allowedValues: ['development', 'production']
+      ComponentParam 'RDSInstanceType'
+      ComponentParam 'RDSAllocatedStorage'
+      ComponentParam 'DnsDomain'
+      maximum_availability_zones.times do |az|
+        ComponentParam "SubnetPersistence#{az}"
+      end
+    end
 end
--- rds-mssql.cfndsl.rb
+++ rds-mssql.cfndsl.rb
@@ -1,6 +1,68 @@
 CloudFormation do
-  ## Add your cfndsl resources here
-  SNS_Topic(:Topic) {
-    TopicName topic_name if defined? topic_name
-  }
-end
+
+    Description "#{component_name} - #{component_version}"
+  
+    az_conditions_resources('SubnetPersistence', maximum_availability_zones)
+  
+    tags = []
+    tags << { Key: 'Environment', Value: Ref(:EnvironmentName) }
+    tags << { Key: 'EnvironmentType', Value: Ref(:EnvironmentType) }
+  
+    extra_tags.each { |key,value| tags << { Key: key, Value: value } } if defined? extra_tags
+  
+    EC2_SecurityGroup "SecurityGroupRDS" do
+      VpcId Ref('VPCId')
+      GroupDescription FnJoin(' ', [ Ref(:EnvironmentName), component_name, 'security group' ])
+      SecurityGroupIngress sg_create_rules(security_group, ip_blocks)
+      Tags tags + [{ Key: 'Name', Value: FnJoin('-', [ Ref(:EnvironmentName), component_name, 'security-group' ])}]
+    end
+  
+    RDS_DBSubnetGroup 'SubnetGroupRDS' do
+      DBSubnetGroupDescription FnJoin(' ', [ Ref(:EnvironmentName), component_name, 'subnet group' ])
+      SubnetIds az_conditional_resources('SubnetPersistence', maximum_availability_zones)
+      Tags tags + [{ Key: 'Name', Value: FnJoin('-', [ Ref(:EnvironmentName), component_name, 'subnet-group' ])}]
+    end
+  
+    RDS_DBParameterGroup 'ParametersRDS' do
+      Description FnJoin(' ', [ Ref(:EnvironmentName), component_name, 'parameter group' ])
+      Family family
+      Parameters parameters if defined? parameters
+      Tags tags + [{ Key: 'Name', Value: FnJoin('-', [ Ref(:EnvironmentName), component_name, 'parameter-group' ])}]
+    end
+
+    instance_username = defined?(master_username) ? master_username : FnJoin('', [ '{{resolve:ssm:', FnSub(master_login['username_ssm_param']), ':1}}' ])
+    instance_password = defined?(master_password) ? master_password : FnJoin('', [ '{{resolve:ssm-secure:', FnSub(master_login['password_ssm_param']), ':1}}' ])
+  
+  
+    RDS_DBInstance 'RDS' do
+      DeletionPolicy deletion_policy if defined? deletion_policy
+      DBInstanceClass Ref('RDSInstanceType')
+      AllocatedStorage Ref('RDSAllocatedStorage')
+      StorageType 'gp2'
+      Engine engine
+      EngineVersion engine_version
+      DBParameterGroupName Ref('ParametersRDS')
+      MasterUsername  instance_username
+      MasterUserPassword instance_password
+      DBSnapshotIdentifier  Ref('RDSSnapshotID')
+      DBSubnetGroupName  Ref('SubnetGroupRDS')
+      VPCSecurityGroups [Ref('SecurityGroupRDS')]
+      MultiAZ Ref('MultiAZ')
+      Tags  tags + [
+        { Key: 'Name', Value: FnJoin('-', [ Ref(:EnvironmentName), component_name, 'instance' ])},
+        { Key: 'SnapshotID', Value: Ref('RDSSnapshotID')},
+        { Key: 'Version', Value: family}
+      ]
+    end
+  
+    record = defined?(dns_record) ? dns_record : 'mssql'
+  
+    Route53_RecordSet('DatabaseIntHostRecord') do
+      HostedZoneName FnJoin('', [ Ref('EnvironmentName'), '.', Ref('DnsDomain'), '.'])
+      Name FnJoin('', [ record, '.', Ref('EnvironmentName'), '.', Ref('DnsDomain'), '.' ])
+      Type 'CNAME'
+      TTL 60
+      ResourceRecords [ FnGetAtt('RDS','Endpoint.Address') ]
+    end
+  
+  end
